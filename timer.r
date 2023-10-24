14bbba23927b7ee6201d2e9202aecefeb7d782f4
angelicavertua <146018290+angelicavertua@users.noreply.github.com>
2023-10-24 11:51:13 +0200

    creation of a new folder called scripts and of 4 files, done 3 methods

---
+++ timer.r
@@ -0,0 +1,19 @@
+library(tictoc)
+
+# Load and time Method 1
+source("scripts/method1.r")
+tic("Method 1")
+# Call the relevant function or script here
+toc()
+
+# Load and time Method 2
+source("scripts/method2.r")
+tic("Method 2")
+# Call the relevant function or script here
+toc()
+
+# Load and time Method 3
+source("scripts/method3.r")
+tic("Method 3")
+# Call the relevant function or script here
+toc()
